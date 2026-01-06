a963fc742bb1f094742377b7a14e3445f5003f83
Jonatan Reginato <jonatan.reginato@gmail.com>
2026-01-06 09:50:10 -0300

    feat(app): enhance exception handling
    - add handling for GitHubSearchLimitException and ValidationException
    - replace raw status codes with Symfony response constants
    - improve clarity and maintainability of exception responses

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Exceptions\GitHubRateLimitException;
+use App\Exceptions\GitHubSearchLimitException;
 use App\Exceptions\GitHubUnavailableException;
 use App\Exceptions\RepositoryNotFoundException;
 use App\Http\Middleware\ForceJsonResponse;
@@ -8,6 +9,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -23,18 +26,27 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->renderable(function (RepositoryNotFoundException $e): JsonResponse {
             return response()->json([
                 'message' => $e->getMessage(),
-            ], 404);
+            ], Response::HTTP_NOT_FOUND);
         });
-
+        $exceptions->renderable(function (ValidationException $e) {
+            return response()->json([
+                'message' => 'Validation error',
+                'errors' => $e->errors(),
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        });
+        $exceptions->renderable(function (GitHubSearchLimitException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        });
         $exceptions->renderable(function (GitHubRateLimitException $e): JsonResponse {
             return response()->json([
                 'message' => $e->getMessage(),
-            ], 429);
+            ], Response::HTTP_TOO_MANY_REQUESTS);
         });
-
         $exceptions->renderable(function (GitHubUnavailableException $e): JsonResponse {
             return response()->json([
                 'message' => $e->getMessage(),
-            ], 502);
+            ], Response::HTTP_SERVICE_UNAVAILABLE);
         });
     })->create();
